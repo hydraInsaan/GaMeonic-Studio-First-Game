7e0d6b9a187d45a8fb326e61199f07a19a426c9b
Yuvraj Vishwakarma <95295134+hydraInsaan@users.noreply.github.com>
2022-06-18 13:13:44 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1654693911,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/player_ship-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"images/player_laser-sheet0.png",
+		"images/rock-sheet0.png",
+		"images/rock-sheet1.png",
+		"images/spammer-sheet0.png",
+		"images/particles.png",
+		"images/particles2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
